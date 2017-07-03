a0f1e773ece5d7f327d38c23ab238fc8a37210d3
Tomasz Ściurka <creativedeveloper-net@users.noreply.github.com>
2017-07-03 22:55:19 +0200

    added move to the immutably-array index file

--- src/immutably-array/immutably-array.js
+++ src/immutably-array/immutably-array.js
@@ -4,8 +4,9 @@ import {shift} from './shift';
 import {unshift} from './unshift';
 import {splice} from './splice';
 import {map} from './map';
+import {move} from './move';
 
-const immutablyArray = {push, pop, shift, unshift, splice, map};
+const immutablyArray = {push, pop, shift, unshift, splice, map, move};
 
 export default immutablyArray;
 export {immutablyArray};
