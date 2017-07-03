8be33e93547ecf7a40a8472022ea941823fe4fd3
Grzegorz Wasiak <g.wasiak@gmail.com>
2017-07-03 23:03:44 +0200

    reduce exposed properly

--- src/immutably-array/immutably-array.js
+++ src/immutably-array/immutably-array.js
@@ -1,12 +1,13 @@
 import {push} from './push';
 import {pop} from './pop';
-import {shift} from './shift';
 import {unshift} from './unshift';
+import {shift} from './shift';
 import {splice} from './splice';
 import {map} from './map';
+import {reduce} from './reduce';
 import {move} from './move';
 
-const immutablyArray = {push, pop, shift, unshift, splice, map, move};
+const immutablyArray = {push, pop, unshift, shift, splice, map, reduce, move};
 
 export default immutablyArray;
 export {immutablyArray};
